b993032f734f9c82b348c047c0abae16a7aac658
akshay.dobariya <akshay.dobariya@nutanix.com>
2022-05-06 04:03:19 +0530

    Added viper to read config from env file

--- main.go
+++ main.go
@@ -6,24 +6,24 @@ import (
 
 	"github.com/akshay-dobariya/simple-bank/api"
 	db "github.com/akshay-dobariya/simple-bank/db/sqlc"
+	"github.com/akshay-dobariya/simple-bank/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load configurations:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatalf("Cannot connect to the DB error: '%s'", err)
 	}
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("Cannot start server:", err)
 	}
